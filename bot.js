28b351892a718f81b84156f6d268231503cc5ba8
cracraftyfty <40735864+cracraftyfty@users.noreply.github.com>
2018-07-02 07:24:55 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,16 +7,8 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'ping') {
-    	message.reply('YO CADET! Dont ever try to type Ping again or U will be Shot in Head!!');
-  
+    	message.reply('Im not gonna say pong bush :>');
+  	}
 });
-    
- client.on('message',message => {
-     if (messsge.content === 'help') {
-         message.reply ('Type ping to get a reply');
-     
-     });
-           // THIS  MUST  BE  THIS  WAY
+
 client.login(process.env.BOT_TOKEN);
-    
-    
